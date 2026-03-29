863afb6b9ac9a1db3c9cbf6359f913446828331e
AKI <akerem.ince@gmail.com>
2026-03-30 00:24:30 +0300

    init

---
+++ backend/notmini/src/main/java/com/akionur/notmini/NotminiApplication.java
@@ -0,0 +1,13 @@
+package com.akionur.notmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotminiApplication.class, args);
+	}
+
+}
---
+++ backend/notmini/src/test/java/com/akionur/notmini/NotminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akionur.notmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
